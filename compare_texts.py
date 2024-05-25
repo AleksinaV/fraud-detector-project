dc98d19fb3787a4f8db53271a27701f098c8ae58
ibitsu <vi.alexina@gmail.com>
2024-05-25 17:06:28 +0300

    Add compare texts

---
+++ compare_texts.py
@@ -0,0 +1,34 @@
+import pickle
+
+import text_checker
+
+
+def collect_data():
+    unpickled_result = pickle.load(open('base_result.pkl', 'rb'))
+    base_result = text_checker.form_result(unpickled_result)
+
+    return base_result
+
+
+def compare_parameters(input_result, base_result):
+    base_words_amount = len(base_result["word_list"])
+    input_words_amount = len(input_result["word_list"])
+
+    base_parameters_amount = count_parameters(base_result)
+    input_parameters_amount = count_parameters(input_result)
+
+    base_coefficient = round((base_parameters_amount / base_words_amount), 2)
+    input_coefficient = round((input_parameters_amount / input_words_amount), 2)
+
+    if input_coefficient >= base_coefficient:
+        return [True, input_coefficient, base_coefficient]
+    else:
+        return [False, input_coefficient, base_coefficient]
+
+
+def count_parameters(dictio):
+    parameters_amount = 0
+    for key in list(dictio.keys())[1::]:
+        parameters_amount += len(dictio[key])
+
+    return parameters_amount
